ca758e707ba03e03e72441186819757f9330ce32
usheikhh <us.sheikhh@gmail.com>
2025-01-05 19:28:23 -0500

    finish preprocessing data

--- main.py
+++ main.py
@@ -21,13 +21,20 @@ ma_200_days = data.Close.rolling(window=200).mean() # 200 days moving average
 # plt.plot(data.Close,'g')
 # plt.show()
 
-
 data_train = pd.DataFrame(data.Close[0:int(len(data)*0.8)]) # 80% of data for training
 data_test = pd.DataFrame(data.Close[int(len(data)*0.8):]) # 20% of data for testing
 
 # print(data_train.shape[0]) # 2216 days for training
 # print(data_test.shape[0]) # 554 days for testing
 
-from sklearn.preprocessing import MinMaxScaler # Normalizing the data
+from sklearn.preprocessing import MinMaxScaler 
 scaler = MinMaxScaler(feature_range=(0,1))
-data_train_scaled = scaler.fit_transform(data_train)
+data_train_scaled = scaler.fit_transform(data_train) # Normalizing the training data
+
+x = []
+y = []
+#taking the first 100 days of data to predict day 101 
+for i in range(100, data_train.shape[0]):
+    x.append(data_train_scaled[i-100:i]) # rolling window of 100 days
+    y.append(data_train_scaled[i,0]) # the next day after 100 days; the target value
+
